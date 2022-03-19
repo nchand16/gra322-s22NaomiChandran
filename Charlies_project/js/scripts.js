3eed951f18f4d66f1f97b4d25bf46c2e194454b3
nchand16 <nchand16@asu.edu>
2022-03-18 17:45:01 -0700

    changes

--- Charlies_project/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$('header').waypoint(function(direction) {
-    if(direction == 'down'){
-      $('header').addClass('header-toggle');
-      $('div').css('paddingTop','52px');
-    }else{
-      $('header').removeClass('header-toggle');
-      $('div').css('paddingTop','0');
-    }
-  });
